77a6267ce9bb6b66132c2a0c1af48ea03e7dfd87
Jason Gorman <email@jasongorman.uk>
2021-11-16 21:28:35 +0000

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,15 @@
+const destinationURL = "https://example.com";
+const statusCode = 301;
+
+async function handleRequest(request) {
+  const userAgent = request.headers.get("User-Agent") || "";
+  if(userAgent.includes('Trident')) {
+    return Response.redirect(destinationURL, statusCode);
+  }
+  return fetch(request);
+}
+
+addEventListener("fetch", async event => {
+  event.passThroughOnException(); // any errors will allow requests to go through as normal
+  event.respondWith( handleRequest(event.request) )
+});
